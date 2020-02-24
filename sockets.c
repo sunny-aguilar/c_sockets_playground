8d6bcf77d23bb6dbc62e455bc30da938a5c42c5f
Sunny Aguilar <sunny.aguilar@gmail.com>
2020-02-23 23:06:19 -0800

    sockets

--- sockets.c
+++ sockets.c
@@ -13,7 +13,7 @@ int main()
 {
     // sock_fd returns the lowest file descriptor available
     int sock_fd = socket(domain, type, protocol);
-    int sock_fd = socket(AF_INET, type, protocol);
+    int sock_fd = socket(AF_INET, SOCK_STREAM, protocol);
     if (sock_fd == -1)
     {
         printf("You got socked!\n");
