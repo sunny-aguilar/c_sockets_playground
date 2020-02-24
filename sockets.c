7eeca968bc7812e1a11c5b9c99c65b851d05d53d
Sunny Aguilar <sunny.aguilar@gmail.com>
2020-02-23 23:40:49 -0800

    sockets

--- sockets.c
+++ sockets.c
@@ -24,7 +24,7 @@ int main()
 
     // int connect(int socket_fc, struct sockaddr* address, size_t address_size);
     int connect(socket_fd, );
-    if ()
+    if (connect())
     {
         perror("Hull breach: connect()");
         exit(1);
