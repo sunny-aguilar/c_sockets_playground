e2dcc50e6cb3e587d4186cdd1a3ed42218cc59ae
Sunny Aguilar <sunny.aguilar@gmail.com>
2020-02-23 23:06:11 -0800

    sockets

--- sockets.c
+++ sockets.c
@@ -12,6 +12,7 @@
 int main()
 {
     // sock_fd returns the lowest file descriptor available
+    int sock_fd = socket(domain, type, protocol);
     int sock_fd = socket(AF_INET, type, protocol);
     if (sock_fd == -1)
     {
