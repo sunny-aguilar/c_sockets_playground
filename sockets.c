795fbb2ea709d12e4f977778b873d80636470a13
Sunny Aguilar <sunny.aguilar@gmail.com>
2020-02-26 23:09:09 -0800

    sockets

--- sockets.c
+++ sockets.c
@@ -61,7 +61,6 @@ int main()
     // ssize_t sent( int socket_fd, void *message, size_t message_size, int flags );
     //ssize_t recv( int socket_fd, void *buffer, size_t buffer_size, int flags );
 
-
     char buffer[1024];
     memset(buffer, '\0', sizeof(buffer));
     r = recv(socket_fd, buffer, sizeof(buffer) - 1, 0);
